315d00c55f82b14cbaa9eee9e5921e81e72cd11c
toubi <wisogetnid@gmail.com>
2016-04-19 22:34:53 +0200

    added recursive order checker

--- src/main/scala/fp/Recursion.scala
+++ src/main/scala/fp/Recursion.scala
@@ -1,13 +1,16 @@
 package fp
 
 object Recursion {
-  def isSorted[A](array: Array[A], intOrder: (A, A) => Boolean) = ???
+  def isSorted[A](array: Array[A], isInOrder: (A, A) => Boolean): Boolean =
+    if (array.length <= 1) true
+    else if (isInOrder(array.head, array.tail.head))
+      isSorted(array.tail.tail, isInOrder)
+    else
+      false
 
-  def fib(n: Int): Int = {
+  def fib(n: Int): Int =
     if (n <= 1)
       n
     else
       fib(n-1) + fib(n-2)
-  }
-
 }
--- src/test/scala/fp/RecursionTest.scala
+++ src/test/scala/fp/RecursionTest.scala
@@ -18,6 +18,30 @@ class RecursionTest extends FlatSpec with Matchers {
   "isSorted validator" should "validate an ordered array of integers" in {
     val intOrder: (Int, Int) => Boolean = _ >= _
 
-    Recursion.isSorted[Int](Array(9, 7, 3, 2, 1), intOrder)
+    Recursion.isSorted[Int](Array(9, 7, 3, 2, 1), intOrder) shouldBe true
+  }
+
+  "isSorted validator" should "notify an unordered array of integers" in {
+    val intOrder: (Int, Int) => Boolean = _ >= _
+
+    Recursion.isSorted[Int](Array(9, 7, 3, 15, 2, 1), intOrder) shouldBe false
+  }
+
+  "isSorted validator" should "validate an ordered array of only one integer" in {
+    val intOrder: (Int, Int) => Boolean = _ >= _
+
+    Recursion.isSorted[Int](Array(9), intOrder) shouldBe true
+  }
+
+  "isSorted validator" should "validate an ordered array of strings" in {
+    val stringOrder: (String, String) => Boolean = _ >= _
+
+    Recursion.isSorted[String](Array("f", "e", "c", "a"), stringOrder) shouldBe true
+  }
+
+  "isSorted validator" should "notify an unordered array of strings" in {
+    val stringOrder: (String, String) => Boolean = _ >= _
+
+    Recursion.isSorted[String](Array("a", "f", "z"), stringOrder) shouldBe false
   }
 }
