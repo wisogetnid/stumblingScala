e09287f5606c8f3abf15145f1b7f8eb7dfcaa338
toubi <wisogetnid@gmail.com>
2016-05-07 12:24:24 +0200

    added max and fold methods to tree

--- src/main/scala/fp/Tree.scala
+++ src/main/scala/fp/Tree.scala
@@ -5,25 +5,29 @@ sealed trait Tree[A] {
   def size: Int
   def maximum(f: (A, A) => A): A
   def map(f: (A) => A): Tree[A]
+  def fold[B](f: A => B)(g: (B, B) => B): B
 }
 
 case class Branch[A](left: Tree[A], right: Tree[A]) extends Tree[A] {
-  override def size: Int = left.size + right.size + 1
-
-  override def maximum(f: (A, A) => A): A =
-    f(left.maximum(f), right.maximum(f))
+  override def size: Int = 1 + left.size + right.size
 
   override def depth: Int = 1 + left.depth + right.depth
 
+  override def maximum(f: (A, A) => A): A = f(left.maximum(f), right.maximum(f))
+
   override def map(f: (A) => A): Tree[A] = Branch(left.map(f), right.map(f))
+
+  override def fold[B](f: A => B)(g: (B, B) => B): B = g(left.fold(f)(g), right.fold(f)(g))
 }
 
 case class Leaf[A](value: A) extends Tree[A] {
   override def size: Int = 1
 
-  override def maximum(f: (A, A) => A): A = value
-
   override def depth: Int = 0
 
+  override def maximum(f: (A, A) => A): A = value
+
   override def map(f: (A) => A): Tree[A] = Leaf(f(value))
+
+  override def fold[B](f: A => B)(g: (B, B) => B): B = f(value)
 }
--- src/test/scala/fp/MyTreeTest.scala
+++ src/test/scala/fp/MyTreeTest.scala
@@ -7,20 +7,35 @@ class MyTreeTest extends FlatSpec with Matchers {
   val tree: Tree[Int] = Branch(Branch(Leaf(1), Leaf(2)), Branch(Branch(Leaf(5), Leaf(4)), Leaf(3)))
 
   "size" should "print the number of all branches and leaves" in {
-    val tree: Tree[Int] = Branch(Branch(Leaf(1), Leaf(2)), Leaf(3))
-    tree.size shouldBe 5
-  }
-
-  "maximum" should "return the maximum value of the tree" in {
-    tree.maximum(_ max _) shouldBe 5
+    tree.size shouldBe 9
   }
 
   "depth" should "return the mazimum path length from root to any leaf" in {
     tree.depth shouldBe 4
   }
 
+  "maximum" should "return the maximum value of the tree" in {
+    tree.maximum(_ max _) shouldBe 5
+  }
+
   "map" should "execute a function with every Leaf of a tree" in {
-    val tree: Tree[Int] = Branch(Branch(Leaf(1), Leaf(2)), Leaf(3))
-    tree.map(_ + 1) shouldBe Branch(Branch(Leaf(2), Leaf(3)), Leaf(4))
+    tree.map(_ + 1) shouldBe Branch(Branch(Leaf(2), Leaf(3)), Branch(Branch(Leaf(6), Leaf(5)), Leaf(4)))
+  }
+
+  "fold" should "be open to calculate size" in {
+    tree.fold(_ => 1)(_ + _ + 1) shouldBe 9
+  }
+
+  it should "be open to calculate depth" in {
+    tree.fold(_ => 0)(_ + _ + 1) shouldBe 4
+  }
+
+  it should "be open to calculate max" in {
+    tree.fold(identity)((a, b) => if (a > b) a else b) shouldBe 5
+  }
+
+  it should "be open to implement map" in {
+//    tree.map(_ + 1)
+    tree.fold(a => Leaf(a + 1): Tree[Int])(Branch(_, _)) shouldBe Branch(Branch(Leaf(2), Leaf(3)), Branch(Branch(Leaf(6), Leaf(5)), Leaf(4)))
   }
 }
