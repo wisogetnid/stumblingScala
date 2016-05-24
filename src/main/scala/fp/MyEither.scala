42bd3c8b56e495c970de0997a3c2cbf5082b1807
toubi <wisogetnid@gmail.com>
2016-05-24 23:43:55 +0200

    add flatMap

--- src/main/scala/fp/MyEither.scala
+++ src/main/scala/fp/MyEither.scala
@@ -1,12 +1,16 @@
 package fp
 
 sealed trait MyEither[+E, +A] {
-  def map[B](f: A => B) = this match {
-    case MyRight(value) => MyRight(f(value))
+  def map[B](f: A => B) = this.flatMap(a => MyRight(f(a)))
+
+  def flatMap[EE >: E,B](f: A => MyEither[EE, B]) = this match {
+    case MyRight(value) => f(value)
     case e => e
   }
+
 }
 
 case class MyLeft[+E](value: E) extends MyEither[E, Nothing]
 
-case class MyRight[+A](value: A) extends MyEither[Nothing, A]
+case class MyRight[+A](value: A) extends MyEither[Nothing, A] {
+}
--- src/test/scala/fp/EitherTest.scala
+++ src/test/scala/fp/EitherTest.scala
@@ -4,7 +4,7 @@ import org.scalatest.{FlatSpec, Matchers}
 
 class EitherTest extends FlatSpec with Matchers {
 
-  "map" should "perform a function of the right value" in {
+  "map" should "perform a function on the right value" in {
     val right = MyRight(3)
     right.map(_ + 4) shouldBe MyRight(7)
   }
@@ -12,5 +12,20 @@ class EitherTest extends FlatSpec with Matchers {
   it should "return left unchanged" in {
     val left: MyEither[String, Int] = MyLeft("some")
     left.map(_ + "thing") shouldBe MyLeft("some")
+  }
+
+  "flatMap" should "perform a function on the right value" in {
+    val right = MyRight("begin")
+    right.flatMap(a => MyRight(a + "ning")) shouldBe MyRight("beginning")
+  }
+
+  it should "return left when a function returning left gets applied on a right value" in {
+    val right = MyRight("right")
+    right.flatMap(a => MyLeft("leftie")) shouldBe MyLeft("leftie")
+  }
+  
+  it should "return left if executed on a left value" in {
+    val left = MyLeft("left")
+    left.flatMap(a => MyRight("good")) shouldBe MyLeft("left")
   }
 }
