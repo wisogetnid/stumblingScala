e2a651e0b2ddbdb849a8ac7cde938a559500f066
toubi <wisogetnid@gmail.com>
2016-05-26 16:28:25 +0200

    add orElse and map2 methods

--- src/main/scala/fp/MyEither.scala
+++ src/main/scala/fp/MyEither.scala
@@ -1,16 +1,21 @@
 package fp
 
 sealed trait MyEither[+E, +A] {
-  def map[B](f: A => B) = this.flatMap(a => MyRight(f(a)))
+  def map[B](f: A => B): MyEither[E, B] = this.flatMap(a => MyRight(f(a)))
 
-  def flatMap[EE >: E,B](f: A => MyEither[EE, B]) = this match {
+  def flatMap[EE >: E, B](f: A => MyEither[EE, B]): MyEither[EE, B] = this match {
     case MyRight(value) => f(value)
-    case e => e
+    case MyLeft(e) => MyLeft(e)
   }
 
+  def orElse[EE >: E, B >: A](b: => B): MyEither[EE, B] = this match {
+    case MyLeft(value) => MyRight(b)
+    case right => right
+  }
+
+  def map2[EE >: E, B, C](b: MyEither[EE, B])(f: (A, B) => C): MyEither[EE, C] = this.flatMap(a => b.map(bb => f(a, bb)))
 }
 
 case class MyLeft[+E](value: E) extends MyEither[E, Nothing]
-
 case class MyRight[+A](value: A) extends MyEither[Nothing, A] {
 }
--- src/test/scala/fp/EitherTest.scala
+++ src/test/scala/fp/EitherTest.scala
@@ -15,17 +15,40 @@ class EitherTest extends FlatSpec with Matchers {
   }
 
   "flatMap" should "perform a function on the right value" in {
-    val right = MyRight("begin")
+    val right: MyEither[String, String] = MyRight("begin")
     right.flatMap(a => MyRight(a + "ning")) shouldBe MyRight("beginning")
   }
 
   it should "return left when a function returning left gets applied on a right value" in {
-    val right = MyRight("right")
+    val right: MyEither[String, String] = MyRight("right")
     right.flatMap(a => MyLeft("leftie")) shouldBe MyLeft("leftie")
   }
-  
+
   it should "return left if executed on a left value" in {
-    val left = MyLeft("left")
+    val left: MyEither[String, String] = MyLeft("left")
     left.flatMap(a => MyRight("good")) shouldBe MyLeft("left")
+  }
+
+  "orElse" should "provide a default case in case of lefts" in {
+    val eitherWithElse: MyEither[String, String] = MyLeft("fail").orElse("nonFail")
+    eitherWithElse shouldBe MyRight("nonFail")
+  }
+
+  it should "not influence a right case" in {
+    val rightWithElse: MyEither[String, String] = MyRight("success").orElse("partialSuccess")
+    rightWithElse shouldBe MyRight("success")
+  }
+
+  "map2" should "combine two rights" in {
+    val aRight = MyRight("happy")
+    val anotherRight = MyRight("hippo")
+    aRight.map2(anotherRight)(_ + " " + _) shouldBe MyRight("happy hippo")
+  }
+
+  it should "return left if one of the arguments is left" in {
+    val right: MyEither[String, String] = MyRight("happy")
+    val left: MyEither[String, String] = MyLeft("sad")
+    right.map2(left)(_ + _) shouldBe MyLeft("sad")
+    left.map2(right)(_ + _) shouldBe MyLeft("sad")
   }
 }
