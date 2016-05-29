4f8a7251283b283f4b2d799e1b289c3dfcfb9baf
toubi <wisogetnid@gmail.com>
2016-05-29 16:06:09 +0200

    added some more methods on MyStream, fixed some non-strictness issues

--- src/main/scala/fp/MyStream.scala
+++ src/main/scala/fp/MyStream.scala
@@ -1,19 +1,46 @@
 package fp
 
+import fp.MyStream.{cons, empty}
+
 sealed trait MyStream[+A] {
-  def takeWhile(f: (A) => Boolean): MyStream[A] = this match {
+  def flatMap[B](f: A => MyStream[B]): MyStream[B] =
+    this.foldRight(empty[B]){ (a, b) => f(a).append(b) }
+
+  def append[AA >: A](as: => MyStream[AA]): MyStream[AA] =
+    this.foldRight(as){ (a, b) => cons(a, b)}
+
+  def filter(p: A => Boolean): MyStream[A] =
+    this.foldRight(empty[A]) { (a, b) => if (p(a)) cons(a, b) else b}
+
+  def map[B](f: A => B): MyStream[B] =
+    this.foldRight(empty[B]) { (a, b) => cons(f(a), b) }
+
+  def headOption: Option[A] =
+    this.foldRight(None: Option[A]) { (a, b) => Some(a) }
+
+  def foldRight[B](z: B)(f: (A, => B) => B): B = this match {
+    case MyCons(h, t) => f(h(), t().foldRight(z)(f))
+    case _ => z
+  }
+
+  def forAll(p: A => Boolean): Boolean = this match {
+    case MyCons(h, t) => p(h()) && t().forAll(p)
+    case _ => true
+  }
+
+  def takeWhile(p: A => Boolean): MyStream[A] = this match {
     case MyEmpty => MyEmpty
-    case MyCons(h, t) => if (f(h())) MyCons(h, () => t().takeWhile(f)) else MyEmpty
+    case MyCons(h, t) => if (p(h())) cons(h(), t().takeWhile(p)) else MyEmpty
   }
 
   def drop(n: Int): MyStream[A] = this match {
     case MyEmpty => MyEmpty
-    case MyCons(h, t) => if (n > 0) t().drop(n - 1) else MyCons(h, t)
+    case MyCons(h, t) => if (n > 0) t().drop(n - 1) else cons(h(), t())
   }
 
   def take(n: Int): MyStream[A] = this match {
     case MyEmpty => MyEmpty
-    case MyCons(h, t) => if (n <= 0) MyEmpty else MyCons(h, () => t().take(n - 1))
+    case MyCons(h, t) => if (n <= 0) MyEmpty else cons(h(), t().take(n - 1))
   }
 
   def toList: List[A] = this match {
@@ -23,6 +50,7 @@ sealed trait MyStream[+A] {
 }
 
 case object MyEmpty extends MyStream[Nothing]
+
 case class MyCons[+A](h: () => A, t: () => MyStream[A]) extends MyStream[A]
 
 object MyStream {
@@ -35,6 +63,6 @@ object MyStream {
   def empty[A]: MyStream[A] = MyEmpty
 
   def apply[A](as: A*): MyStream[A] = {
-    if (as.isEmpty) MyEmpty else MyCons(() => as.head, () => apply(as.tail: _*))
+    if (as.isEmpty) MyEmpty else cons(as.head, apply(as.tail: _*))
   }
 }
--- src/test/scala/fp/MyStreamTest.scala
+++ src/test/scala/fp/MyStreamTest.scala
@@ -1,26 +1,67 @@
 package fp
 
+import fp.MyStream.cons
 import org.scalatest.{FlatSpec, Matchers}
 
-class MyStreamTest extends FlatSpec with Matchers{
+class MyStreamTest extends FlatSpec with Matchers {
+  private val stream: MyStream[Int] = MyStream(1, 2, 3)
 
   "toList" should "convert a stream into a List" in {
-    val stream = MyStream(1, 2, 3)
     stream.toList shouldBe List(1, 2, 3)
   }
 
   "take(n)" should "return the first n elements" in {
-    val stream = MyStream(1, 2, 3)
     stream.take(2).toList shouldBe List(1, 2)
   }
 
   "drop(n)" should "drop the first n elements" in {
-    val stream = MyStream(1, 2, 3)
     stream.drop(2).toList shouldBe List(3)
   }
 
   "takeWhile" should "return leading elements of a stream while the predicate matches" in {
     val stream = MyStream(1, 3, 4, 5)
     stream.takeWhile(_ % 2 == 1).toList shouldBe List(1, 3)
+  }
+
+  "forall" should "validate a predicate on every element of a stream" in {
+    val stream = MyStream(1, 3, 5)
+    stream.forAll(_ % 2 == 1) shouldBe true
+  }
+
+  it should "return false (and terminate) on the first non matching element" in {
+    val stream = cons(1, cons(2, cons({ fail("should not be executed"); 3}, cons(5, MyStream.empty))))
+    //    val stream = MyStream(1, 2, {fail(); 3}, 5) doesn't work as all the elements get evaluated on instantiation
+    stream.forAll(_ % 2 == 1) shouldBe false
+  }
+
+  "foldRight" should "do what it's supposed to" in {
+    stream.foldRight(10)(_ + _) shouldBe 16
+  }
+
+  "headOption" should "return the first element of a stream" in {
+    stream.headOption shouldBe Some(1)
+  }
+
+  it should "return None is no elements are in the stream" in {
+    val stream: MyStream[Any] = MyEmpty
+    stream.headOption shouldBe None
+  }
+
+  "map" should "execute a function on every element" in {
+    stream.map(_ + 1).toList shouldBe List(2, 3, 4)
+  }
+
+  "filter" should "remove all elements not matching a predicate" in {
+    stream.filter(_ % 2 == 0).toList shouldBe List(2)
+  }
+
+  "append" should "append a stream to another" in {
+    val anotherStream = MyStream(4, 5)
+    stream.append(anotherStream).toList shouldBe List(1, 2, 3, 4, 5)
+  }
+
+  "flatMap" should "apply a function returning streams on each element of a stream and flatten the result" in {
+    val wordStream = MyStream("bob", "eve")
+    wordStream.flatMap(a => MyStream(a.split(""): _*)).toList shouldBe List("b", "o", "b", "e", "v", "e")
   }
 }
